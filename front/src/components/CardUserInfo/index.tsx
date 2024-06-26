1acff8dbbde8e81a1545a7a4ff743603fac32d14
adriellyb <adrielly0195@gmail.com>
2024-06-26 09:11:34 -0300

    front: card de boas vindas ao usuário

--- front/src/components/CardUserInfo/index.tsx
+++ front/src/components/CardUserInfo/index.tsx
@@ -1,10 +1,76 @@
+import { Card } from "primereact/card";
+import { Image } from "primereact/image";
+import { Divider } from "primereact/divider";
+
+import "./style.css";
+import img from "../../assets/girl.png"
+import { Button } from "primereact/button";
+
 export default function CardUserInfo(data: any) {
-    
-    const user  = data.data;    
-    
+
+    const user = data.data;
+
+    const dataHora = () => {
+        const data = new Date();
+        const dias = [
+            "Domingo",
+            "Segunda-feira",
+            "Terça-feira",
+            "Quarta-feira",
+            "Quinta-feira",
+            "Sexta-feira",
+            "Sábado"
+        ]
+
+        const mes = data.getMonth() + 1;
+        let hora = data.getHours() <= 9 ? "0"+data.getHours() : data.getHours();
+        let minutos = data.getMinutes() <= 9 ? "0"+data.getMinutes() : data.getMinutes();
+
+        const dataAtualizada = {
+            dataHoje: data.getDate() + "/" + mes + "/" + data.getFullYear(),
+            horaAgora: hora + ":" + minutos,
+            diaSemana: dias[data.getDay()]
+        }
+
+        return dataAtualizada;
+    }
+
     return (
         <>
-        <p>{ user.nome }</p>
+            <Card className="p-0">
+                <div className="flex flex-wrap">
+                    <div className="date">
+                        <h2>{dataHora().horaAgora}</h2>
+                        <p>{dataHora().diaSemana + ", " + dataHora().dataHoje}</p>
+                    </div>
+                    <Divider layout="vertical" />
+
+                    <div className="info-box flex flex-wrap">
+                        <Image
+                            src={img}
+                            alt="menina apontando para a direita"
+                            width="120"
+                        />
+                        <div className="texto">
+                            <p>Bem vindo(a), {user.nome}</p>
+                            <p className="text-sm">Não esqueça de manter seu saldo e suas despesas atualizados.</p>
+                            <div className="flex">
+                                <Button
+                                    label="Editar dados"
+                                    icon="pi pi-user-edit"
+                                    size="small"
+                                />
+                                <Button
+                                    icon="pi pi-sign-out"
+                                    size="small"
+                                    severity="danger"
+                                    className="mx-3"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Card>
         </>
     )
 }
--- front/src/pages/Dashboard/Dashboard.tsx
+++ front/src/pages/Dashboard/Dashboard.tsx
@@ -3,75 +3,91 @@ import api from "../../services/api";
 
 import CardUserInfo from "../../components/CardUserInfo";
 import useAuth from "../../contexts/auth";
+import { Card } from "primereact/card";
 
 function Dashboard() {
-    
-    const [ userData, setUserData ]:any = useState([]);
-    const [ saldo, setSaldo ]:any = useState([]);
-    const [ despesas, setDespesas ]:any = useState([]);
 
-    const token = "Bearer "+localStorage.getItem("token");
+    const [userData, setUserData]: any = useState([]);
+    const [saldo, setSaldo]: any = useState([]);
+    const [despesas, setDespesas]: any = useState([]);
+
+    const token = "Bearer " + localStorage.getItem("token");
     const userId = localStorage.getItem("id");
 
-    const getDetails = async() => {
+    const getDetails = async () => {
         const headers = {
             'headers': {
                 'Authorization': token,
             }
         };
         await api.
-        get("/auth/getDetails", headers).
+            get("/auth/getDetails", headers).
             then((res) => {
                 const { sub } = res.data.user;
                 localStorage.setItem("id", sub);
 
                 setUserData(res.data.user);
-                
+
             }).catch((err) => {
                 console.error("Ocorreu um erro: " + err);
             });
     }
 
-    const getSaldo = async() => {
+    const getSaldo = async () => {
         await api.
-        get(`/saldo/usuario/${userId}`).
+            get(`/saldo/usuario/${userId}`).
             then((res) => {
                 setSaldo(res.data.saldo);
                 console.log(res);
-                
+
             }).catch((err) => {
                 console.error("Ocorreu um erro: " + err);
             });
     }
 
-    const getDespesas = async() => {
+    const getDespesas = async () => {
         await api.
-        get(`/despesa/usuario/${userId}`).
+            get(`/despesa/usuario/${userId}`).
             then((res) => {
                 setDespesas(res.data.despesas);
                 console.log(res);
-                
+
             }).catch((err) => {
                 console.error("Ocorreu um erro: " + err);
             });
     }
 
-    useEffect(() => {        
+    useEffect(() => {
         getDetails();
     }, []);
 
-    useEffect(() => {  
-        getSaldo();        
-    }, []);
+    // useEffect(() => {  
+    //     getSaldo();        
+    // }, []);
 
-    useEffect(() => {  
-        getDespesas();        
-    }, []);
+    // useEffect(() => {  
+    //     getDespesas();        
+    // }, []);
 
-    return(
+    return (
         <>
-            <CardUserInfo data={userData} />
-            <p>{userId}</p>
+            <div className="grid">
+                <div className="col-12 xl:col-8 p-3">
+                    <CardUserInfo data={userData} />
+                    <br />
+                    <Card />
+                </div>
+                <div className="col-12 xl:col-4 p-3">
+                    <div className="grid">
+                        <div className="col-12 md:col-6 xl:col-12">
+                            <Card title="Meu saldo" subTitle="Última atualização:" />
+                        </div>
+                        <div className="col-12 md:col-6 xl:col-12">
+                            <Card title="Minhas despesas" subTitle="Despesas mais recentes" />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
